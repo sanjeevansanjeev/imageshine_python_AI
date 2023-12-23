5644a6e7cf95446ce3a452136901697aa3867bfb
sanjeevansanjeev <sanjeevanvengatesan@gmail.com>
2023-12-23 21:30:17 +0530

    modified

--- facedetected.py
+++ facedetected.py
@@ -8,7 +8,7 @@ while True:
    blacknwhite=cv2.cvtColor(video,cv2.COLOR_BGR2GRAY)
    face=trainedfacemodel.detectMultiScale(blacknwhite)
    for(x,y,w,h) in face:
-     cv2.rectangle(video,(x,y),(x+w,y+h),(0,255,0),2)
+     cv2.rectangle(video,(x,y),(x+w,y+h),(255,0,0),2)
      cv2.imshow("showingimgae",video)
      Key=cv2.waitKey(1)
      if (Key==32):
